b67ef43b3bcb9948d9fe699bda1cbf821124b0e1
Boris <boris@email.com>
2021-05-05 08:39:27 +1000

    functionality almost ready

---
+++ src/components/ProductList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Product from './Product';
+import Title from './Title';
+import {ProductConsumer} from '../context';
+
+export default class ProductList extends Component {
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='py-5'>
+                    <div className='container'>
+                        <Title name='our' title='products'/>
+                        <div className='row'>
+                            <ProductConsumer>
+                                {(value)=>{
+                                    return (
+                                        value.products.map( p=>{
+                                            return <Product key={p.id} product={p}  />
+                                        } )
+                                    )
+                                }}
+                            </ProductConsumer>
+                            {/* <Title name='our' title='products'/> */}
+                        </div>
+
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
